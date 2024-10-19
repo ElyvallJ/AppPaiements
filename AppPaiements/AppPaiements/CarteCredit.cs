35513ef1f9965c9e9981d7e3237e7f011c8e150e
Elyvall Jiddou <elyvall.jiddou@monboreal.ca>
2024-10-19 09:09:56 -0400

    Third version

---
+++ AppPaiements/AppPaiements/CarteCredit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        // la propriete
+        public int NumeroCarte { get; set; }
+
+        //le constructeur
+        public CarteCredit(int numeroCarte, double montant, string description) : base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        // la methode
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le numéro de la carte est {NumeroCarte},le montant est {Montant} et la description est {Description}.");
+        }
+    }
+}
--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        // les instances de la classe CarteCredit
+        CarteCredit carteCredit1 = new CarteCredit(30013, 10000, "L'argent de l'école");
+        CarteCredit carteCredit2 = new CarteCredit(45213, 5000, "L'argent du nouritture");
+        CarteCredit carteCredit3 = new CarteCredit(96313, 1500, "L'argent de loisir");
         
+        //Afficher les details de ces objets
+        List<CarteCredit> CarteCredits = new List<CarteCredit> { carteCredit1, carteCredit2, carteCredit3 };
+        Console.WriteLine("les cartes de credits sont :");
+        foreach (var CarteCredit in CarteCredits)
+        {
+            CarteCredit.AfficherDetails();
+        }
+
     }
 }
