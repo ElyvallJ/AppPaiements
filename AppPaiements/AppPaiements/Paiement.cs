e771e2cae8e7d13a69aad447aa0d6217f6f1f69c
Elyvall Jiddou <elyvall.jiddou@monboreal.ca>
2024-10-19 08:58:02 -0400

    Second version

---
+++ AppPaiements/AppPaiements/Paiement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        // la propriété
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        //Un constructeur
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+
+        //Une methode
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"le montant est {Montant} et ça description est {Description}");
+        }
+    }
+}
